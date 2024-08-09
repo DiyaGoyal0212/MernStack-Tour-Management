5f1baa33053f6e64fc94bbe324bea06e65f19bb3
DiyaGoyal0212 <goyal.diya0212@gmail.com>
2024-08-09 12:29:41 +0530

    home

--- tour-management/frontend/src/components/Header/Header.jsx
+++ tour-management/frontend/src/components/Header/Header.jsx
@@ -10,7 +10,7 @@ const nav_links =[{
   display:'Home'
 },
 {
-  path:'#',
+  path:'/about',
   display:'About'
 },
 {
@@ -54,7 +54,9 @@ const Header = () => {
               {
                 nav_links.map((item,index) =>(
                   <li className="nav__item" key={index}>
-                    <NavLink to={item.path}>{item.display}</NavLink>
+                    <NavLink to={item.path}  className={
+                      navClass=> navClass.isActive? "active__link" : ''}>
+                        {item.display}</NavLink>
                   </li>
                 ))
               }
--- tour-management/frontend/src/pages/Homee.jsx
+++ tour-management/frontend/src/pages/Homee.jsx
@@ -1,10 +1,54 @@
 import React from 'react'
+import '../styles/home.css'
+import { Container,Row,Col } from 'reactstrap'
+import heroImg from '../assets/images/hero-img01.jpg'
+import heroImg02 from '../assets/images/hero-img02.jpg'
+import heroVideo from '../assets/images/hero-video.mp4'
+import Subtitle from '../shared/Subtitle'
+import worldImg from '../assets/images/world.png'
+import SearchBar from '../shared/SearchBar'
 
 const Homee = () => {
   return (
-    <div>
-      
-    </div>
+    <>   
+     <section>
+      <Container>
+        <Row>
+          <Col lg='6'>
+          <div className="hero__content">
+            <div className="hero__subtitle d-flex align-items-center">
+              <Subtitle subtitle={'Your ChatGPT for your tour'} />
+              <img src={worldImg} alt="" />
+            </div>
+            <h1>Travelling is the only way you can create great <span className='highlight'>memories</span></h1>
+            <p>Create unforgettable experiences with our comprehensive tour management solution.From booking to billing, we've got your tour business covered.
+            Streamline your operations and delight your customers.</p>
+          </div>
+          </Col>
+
+          <Col>
+          <div className="hero__img-box">
+            <img src={heroImg} alt='' />
+          </div>
+          </Col>
+          <Col>
+          <div className="hero__img-box mt-4">
+            <video src={heroVideo} alt='' controls />
+          </div>
+          </Col>
+          <Col>
+          <div className="hero__img-box mt-5">
+            <img src={heroImg02} alt='' />
+          </div>
+          </Col>
+
+          <SearchBar />
+
+        </Row>
+      </Container>
+
+    </section>
+    </>
   )
 }
 
