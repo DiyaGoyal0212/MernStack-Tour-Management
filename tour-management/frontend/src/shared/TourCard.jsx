32a27c862590469c509099e19d88ace19e12764a
DiyaGoyal0212 <goyal.diya0212@gmail.com>
2024-08-14 23:42:51 +0530

    tour

--- tour-management/frontend/src/shared/TourCard.jsx
+++ tour-management/frontend/src/shared/TourCard.jsx
@@ -21,6 +21,14 @@ const {id,title,city,photo,price,featured,avgRating,reviews} = tour;
         <span className="tour__location d-flex align-items-center gap-1">
         <i class="ri-map-pin-line"></i>{city}
         </span>
+        <span className="tour__rating d-flex align-items-center gap-1">
+        <i class="ri-star-fill"></i>{avgRating}<span>({reviews.length})</span>
+        </span>
+      </div>
+
+      <h5 className="tour__title"><Link to={`/tours/${id}`}>{title}</Link></h5>
+      <div className="card__bottom d-flex align-items-center justify-content-between mt-3">
+          <h5>${price} <span>/per person</span></h5>
       </div>
     </CardBody>
   </div>
